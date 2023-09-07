b2374d0ec40b65cfabfee9eb736a1df955ebc7cc
AhmedI999 <ahmedibrahim5182@gmail.com>
2023-09-07 14:40:05 -0700

    task 1 omg

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
- *
- * @b: allocated memory
- *
- * Return: a pointer to the allocated memory
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or null
  */
+
 void *malloc_checked(unsigned int b)
 {
-	void *i;
+	int *m = malloc(b)
 
-	i = malloc(b);
-	if (i == NULL)
+	if (m == 0)
 		exit(98);
-	return (i);
+	return (m);
 
 }
